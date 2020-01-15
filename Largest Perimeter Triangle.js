b3aafe8078ea9e619b6cdd12555f91107fff0ca4
nysa <msun698@gmail.com>
2020-01-15 21:01:50 +0100

    Did one

---
+++ Largest Perimeter Triangle.js
@@ -0,0 +1,11 @@
+//Given an array A of positive lengths, return the largest perimeter of a triangle with non-zero area, formed from 3 of these
+//lengths.
+
+//If it is impossible to form any triangle of non-zero area, return 0.
+
+//My solution
+var largestPerimeter = function(A) {
+    peri = A.sort((a, b) => b - a);
+    while(peri[0] >= peri[1] + peri[2]) peri.shift();
+    return peri.length < 3 ? 0 : peri[0] + peri[1] + peri[2];
+};
