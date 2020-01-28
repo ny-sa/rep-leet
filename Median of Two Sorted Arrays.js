9b683ad582c52494b1ba23de3252028e23d483d0
nysa <msun698@gmail.com>
2020-01-28 13:32:51 +0100

    little by little

--- Median of Two Sorted Arrays.js
+++ Median of Two Sorted Arrays.js
@@ -21,11 +21,26 @@ The median is (2 + 3)/2 = 2.5*/
 var findMedianSortedArrays = function(nums1, nums2) {
     if (nums1.length + nums2.length === 2) return (nums1[0]+nums2[0])/2;
     let half = Math.floor((nums1.length + nums2.length)/2), lowerHalfFound = false;
-    element1 = Math.floor(Math.random() * nums1.length), element2 = Math.floor(Math.random() * nums2.length);
+        ele1 = Math.floor(Math.random() * nums1.length), 
+        ele2 = Math.floor(Math.random() * nums2.length);
+        filled;
     while (!lowerHalfFound) {
-        if (element1+element2+2 < half) {
-            if (nums1[element2] > num1[element1])
+        switch (filled = ele1+ele2+2) {
+            case (filled < half): 
+                if (nums2[ele2] > num1[ele1])
+                    ele1 += half - filled;
+                else ele2 += half - filled;
+                break;
+            case (filled > half):
+                if (nums2[ele2] > num1[ele1])
+                    ele2 -= half - filled;
+                else ele1 += half - filled;
+                break;
+            default:
+                
+                break;
         }
+
     }
 };
-console.log(findMedianSortedArrays([1,3,5,9],[2,4,6,8]))
+console.log(findMedianSortedArrays([1,2,3,4],[5,6,7,8]))
