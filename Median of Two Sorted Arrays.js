735fd343652d2190f49ffd4edcab070c5b99a2b8
nysa <msun698@gmail.com>
2020-02-02 19:46:09 +0100

    Finally finish 1 hard challenge, and add 4 more

--- Median of Two Sorted Arrays.js
+++ Median of Two Sorted Arrays.js
@@ -19,28 +19,74 @@ The median is (2 + 3)/2 = 2.5*/
 
 //My solution
 var findMedianSortedArrays = function(nums1, nums2) {
+    if (nums1.length === 0) return (nums2.length % 2 === 1) ? nums2[Math.ceil((nums2.length-1)/2)] : 
+        (nums2[nums2.length/2]+nums2[nums2.length/2-1])/2
+    if (nums2.length === 0) return (nums1.length % 2 === 1) ? nums1[Math.ceil((nums1.length-1)/2)] : 
+        (nums1[nums1.length/2]+nums1[nums1.length/2-1])/2
     if (nums1.length + nums2.length === 2) return (nums1[0]+nums2[0])/2;
-    let half = Math.floor((nums1.length + nums2.length)/2), lowerHalfFound = false;
-        ele1 = Math.floor(Math.random() * nums1.length), 
-        ele2 = Math.floor(Math.random() * nums2.length);
-        filled;
-    while (!lowerHalfFound) {
-        switch (filled = ele1+ele2+2) {
-            case (filled < half): 
-                if (nums2[ele2] > num1[ele1])
-                    ele1 += half - filled;
-                else ele2 += half - filled;
-                break;
-            case (filled > half):
-                if (nums2[ele2] > num1[ele1])
-                    ele2 -= half - filled;
-                else ele1 += half - filled;
-                break;
-            default:
-                
-                break;
-        }
 
+    let totalLength = nums1.length + nums2.length,
+        half = Math.ceil(totalLength/2),
+        ele1 = Math.floor(Math.random() * nums1.length), 
+        ele2 = Math.floor(Math.random() * nums2.length),
+        filled = 0;
+
+    while (true) {
+        filled = ele1+ele2+2;
+        if (filled < half) {    //select an index for each array such that they are evenly divided when combined
+            if (nums2[ele2] > nums1[ele1]) {
+                ele1 += half - filled;
+                if (ele1 >= nums1.length) {ele2 += ele1+1-nums1.length; ele1 = nums1.length-1}
+            }
+            else {
+                ele2 += half - filled;
+                if (ele2 >= nums2.length) {ele1 += ele2+1-nums2.length; ele2 = nums2.length-1}
+            } 
+        }
+        else if (filled > half) {
+            if (nums2[ele2] > nums1[ele1]) {
+                ele2 -= filled - half;
+                if (ele2 < -1) {ele1 -= -ele2-1; ele2 = -1;}
+            }
+            else {
+                ele1 -= filled - half;
+                if (ele1 < -1) {ele2 -= -ele1-1; ele1 = -1;}
+            }
+        }
+        else {  //determine if the lower half of a combined array actually has all lower numbers
+            if (ele1 === -1) {
+                if (nums1[0] >= nums2[ele2])
+                    return (totalLength % 2 === 1) ? nums2[ele2] : (nums2[ele2+1]) ? 
+                        (nums2[ele2] + Math.min(nums2[ele2+1], nums1[0]))/2 : (nums2[ele2] + nums1[0])/2;
+                else {ele1++; ele2--;}
+            }
+            else if (ele2 === -1) {
+                if (nums2[0] >= nums1[ele1])
+                    return (totalLength % 2 === 1) ? nums1[ele1] : (nums1[ele1+1]) ? 
+                        (nums1[ele1] + Math.min(nums1[ele1+1], nums2[0]))/2 : (nums1[ele1] + nums2[0])/2;
+                else {ele2++; ele1--;}
+            }
+            else if (ele1 === nums1.length-1) {
+                if (nums1[ele1] <= nums2[ele2+1])
+                    return (totalLength % 2 === 1) ? Math.max(nums1[ele1], nums2[ele2]) : 
+                        (Math.max(nums1[ele1], nums2[ele2]) + nums2[ele2+1])/2;
+                else {ele1--; ele2++;} 
+            }
+            else if (ele2 === nums2.length-1) {
+                if (nums2[ele2] <= nums1[ele1+1])
+                    return (totalLength % 2 === 1) ? Math.max(nums1[ele1], nums2[ele2]) : 
+                        (Math.max(nums1[ele1], nums2[ele2]) + nums1[ele1+1])/2;
+                else {ele2--; ele1++;}
+            }
+            else {
+                if (nums1[ele1] <= nums2[ele2+1]) {
+                    if (nums2[ele2] <= nums1[ele1+1]) 
+                        return (totalLength % 2 === 1) ? Math.max(nums1[ele1], nums2[ele2]) : 
+                            (Math.max(nums1[ele1], nums2[ele2]) + Math.min(nums1[ele1+1], nums2[ele2+1]))/2;
+                    else {ele2--; ele1++;}
+                }
+                else {ele1--; ele2++;}
+            }
+        }
     }
-};
-console.log(findMedianSortedArrays([1,2,3,4],[5,6,7,8]))
+}
